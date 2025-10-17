e7060eba1cdf8ed84e9fa7bbf6c60b18fd82f344
Md Yousuf Zaman <usufzaman059@gmail.com>
2025-10-17 23:00:50 +0600

    chore: add @nuxt/image and @primevue/nuxt-module dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  css: ['assets/css/tailwind.css'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+    '@nuxt/image'
+  ],
+
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: 'Aura'
+      }
+    }
+  },
+
+  image: {
+    quality: 80,
+    format: ['webp', 'avif', 'jpeg'],
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536
+    }
+  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: { head: { title: 'Nuxt Blog' } }
